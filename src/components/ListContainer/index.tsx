1629651f04fa4ef884e9908f0caa34cb02005cdf
Gabriel Matos Lourenço <matos.gabriel2013@gmail.com>
2022-11-17 15:42:41 -0300

    contriundo a logica dos hooks

--- src/components/ListContainer/index.tsx
+++ src/components/ListContainer/index.tsx
@@ -1,14 +1,37 @@
 import { Trash, PlusCircle } from "phosphor-react";
+import { useState } from "react";
 import styles from "./ListContainer.module.css";
 
-
+interface taskProps{
+    id: number;
+    text: string;
+    checked: boolean;
+}
 
 export function ListContainer() {
+   const [task, setTask] = useState<taskProps[]>([]);
+   const [text, setText] = useState("");
+
+   function handleAddTask(text:any){
+    setTask([])
+   }
+
+   
+
     return (
         <main className={styles.container}>
             <header className={styles.inputContainer}>
-                <input placeholder="Adicione uma nova tarefa" />
-                <button>Criar <PlusCircle size={18} weight="bold" /></button>
+                <input 
+                value={text} 
+                onChange={(e) => setText(e.target.value)}
+                placeholder="Adicione uma nova tarefa" type="text"/>
+                <button onClick={() => { 
+                setText("");
+                handleAddTask(text);
+                }}>
+                    Criar
+                    <PlusCircle size={18} weight="bold" />
+                </button>
             </header>
 
             <div className={styles.contentList}>
@@ -27,13 +50,12 @@ export function ListContainer() {
                 <ul>
                     <li>
                       <div className={styles.tasksContainer}>
-                        <label className="checkbox-container">
+                        <label>
                             <input type="checkbox"/>
-                            <span className="checkmark"></span>
                         </label>
                         <p>Integer urna interdum massa libero auctor neque turpis turpis semper. Duis vel sed fames integer.</p>
                         <button type="button" className={styles.trashButton}>
-                        <Trash size={18} weight="bold" />
+                            <Trash size={18} weight="bold" />
                         </button>
                       </div>
                     </li>
