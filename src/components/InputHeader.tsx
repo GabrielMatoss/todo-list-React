9c5a09b9ada8b7958fdcd68ef0cfbc4edcc3c3cb
Gabriel Matos Lourenço <matos.gabriel2013@gmail.com>
2022-11-27 16:36:14 -0300

    bug fixs

--- src/components/InputHeader.tsx
+++ src/components/InputHeader.tsx
@@ -3,18 +3,25 @@ import styles from "../styles/InputHeader.module.css";
 
 
 export function InputHeader({text, setText, handleAddTask}: any){
+function testeSubmit(){
+    
+    event?.preventDefault();
+   
+}
+
     return(
-        <header className={styles.inputContainer}>
+        <form onSubmit={testeSubmit} className={styles.inputContainer}>
              <input
              value={text}
              onChange={(e) => setText(e.target.value)}
              placeholder="Adicione uma nova tarefa" type="text"
+             required
              />
         <button type="submit" onClick={handleAddTask}>
            Criar
           <PlusCircle size={18} weight="bold" />
          </button>
-    </header>
+    </form>
     )
 }
 
