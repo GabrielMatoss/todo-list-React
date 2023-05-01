9684a53e5c7d8fbc8119250f2649bf74b54670c5
Gabriel Matos Lourenço <matos.gabriel2013@gmail.com>
2023-05-01 13:41:07 -0300

    feat(listcontainer.tsx): using the uuid library as a random task id

--- src/components/ListContainer.tsx
+++ src/components/ListContainer.tsx
@@ -1,111 +1,123 @@
-
 import { useState, FormEvent, ChangeEvent, InvalidEvent } from "react";
-import { PlusCircle} from "phosphor-react";
+import { v4 as uuidv4 } from 'uuid';
+import { PlusCircle } from "phosphor-react";
 import { Tasks } from "./Tasks";
 
 import imgClipBoard from "../assets/Clipboard.svg";
 import styles from "../styles/ListContainer.module.css";
 
-
 export interface ListContainerProps {
-    id: number;
-    title: string;
-    checked: boolean;
+  id: string;
+  title: string;
+  checked: boolean;
 }
 
 export function ListContainer() {
-    const [task, setTask] = useState<ListContainerProps[]>([]);
-    const [text, setText] = useState<string>("");
+  const [task, setTask] = useState<ListContainerProps[]>([]);
+  const [text, setText] = useState<string>("");
 
-    function handleAddTask(event: FormEvent) {
-        event?.preventDefault();
+  function handleAddTask(event: FormEvent) {
+    event?.preventDefault();
 
-        if (!text) return;
+    if (!text) return;
 
-        setTask([...task, {
-            id: Math.random(),
-            title: text,
-            checked: false
-        }]);
+    setTask([
+      ...task,
+      {
+        id: uuidv4(),
+        title: text,
+        checked: false,
+      },
+    ]);
 
-        setText("");
-    }
+    setText("");
+  }
 
-    function handleToggleTask(id: number) {
-        const newTasksChecked = task.map(task => task.id === id ? {
+  function handleToggleTask(id: string) {
+    const newTasksChecked = task.map((task) =>
+      task.id === id
+        ? {
             ...task,
-            checked: !task.checked
-        } : task);
+            checked: !task.checked,
+          }
+        : task
+    );
 
-        setTask(newTasksChecked)
-    }
+    setTask(newTasksChecked);
+  }
 
-    function handleRemoveTask(id: number) {
-        const removeTasksFiltered = task.filter(task => task.id !== id);
+  function handleRemoveTask(id: string) {
+    const removeTasksFiltered = task.filter((task) => task.id !== id);
 
-        setTask(removeTasksFiltered);
-    }
+    setTask(removeTasksFiltered);
+  }
 
-    function handleNewCommentChange(event: ChangeEvent<HTMLInputElement>) {
-        event.target.setCustomValidity("");
-        setText(event.target.value);
-    }
+  function handleNewCommentChange(event: ChangeEvent<HTMLInputElement>) {
+    event.target.setCustomValidity("");
+    setText(event.target.value);
+  }
 
-    function handleInvalidComment(event: InvalidEvent<HTMLInputElement>) {
-        event.target.setCustomValidity("Preencha este Campo!");
-    }
+  function handleInvalidComment(event: InvalidEvent<HTMLInputElement>) {
+    event.target.setCustomValidity("Preencha este Campo!");
+  }
 
-    const tasksChecked = task.filter(task => task.checked === true);
+  const tasksChecked = task.filter((task) => task.checked === true);
 
-    return (
-        <main className={styles.container}>
-            <form onSubmit={handleAddTask} className={styles.inputContainer}>
-                <input
-                    value={text}
-                    placeholder="Adicione uma nova tarefa" type="text"
-                    onChange={handleNewCommentChange}
-                    onInvalid={handleInvalidComment}
-                    required
-                />
-                <button type="submit">
-                    Criar
-                    <PlusCircle size={18} weight="bold" />
-                </button>
-            </form>
-            <div className={styles.contentList}>
-                <div className={styles.countSection}>
-                    <section>
-                        <span>Tarefas criadas</span>
-                        <div>{task.length}</div>
-                    </section>
+  return (
+    <main className={styles.container}>
+      <form onSubmit={handleAddTask} className={styles.inputContainer}>
+        <input
+          value={text}
+          placeholder="Adicione uma nova tarefa"
+          type="text"
+          onChange={handleNewCommentChange}
+          onInvalid={handleInvalidComment}
+          required
+        />
+        <button type="submit">
+          Criar
+          <PlusCircle size={18} weight="bold" />
+        </button>
+      </form>
+      <div className={styles.contentList}>
+        <div className={styles.countSection}>
+          <section>
+            <span>Tarefas criadas</span>
+            <div>{task.length}</div>
+          </section>
 
-                    <section>
-                        <span>Concluídas</span>
-                        {
-                            task.length === 0 ?
-                                <div>{task.length}</div> :
-                                <div>{tasksChecked.length} de {task.length}</div>
-                        }
-                    </section>
-                </div>
-                {task.length === 0 ?
-                    (<ul className={task.length === 0 ? styles.listTaskEmpty : ""}>
-                        <div>
-                            <img src={imgClipBoard} alt="Lista vazia" />
-                            <h3>Você ainda não tem tarefas cadastradas</h3>
-                            <p>Crie tarefas e organize seus itens a fazer</p>
-                        </div>
-                    </ul>) : (<ul>{task.map(task => (
-                        <Tasks 
-                        task={task} 
-                        key={task.id} 
-                        handleRemoveTask={handleRemoveTask} 
-                        handleToggleTask={handleToggleTask} 
-                        />
-                    ))}
-                    </ul>)
-                }
+          <section>
+            <span>Concluídas</span>
+            {task.length === 0 ? (
+              <div>{task.length}</div>
+            ) : (
+              <div>
+                {tasksChecked.length} de {task.length}
+              </div>
+            )}
+          </section>
+        </div>
+        {task.length === 0 ? (
+          <ul className={task.length === 0 ? styles.listTaskEmpty : ""}>
+            <div>
+              <img src={imgClipBoard} alt="Lista vazia" />
+              <h3>Você ainda não tem tarefas cadastradas</h3>
+              <p>Crie tarefas e organize seus itens a fazer</p>
             </div>
-        </main>
-    )
+          </ul>
+        ) : (
+          <ul>
+            {task.map((task) => (
+              <Tasks
+                task={task}
+                key={task.id}
+                handleRemoveTask={handleRemoveTask}
+                handleToggleTask={handleToggleTask}
+              />
+            ))}
+          </ul>
+        )}
+      </div>
+    </main>
+  );
 }
